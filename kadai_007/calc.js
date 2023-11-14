693f8e559399f5e0cd40a9a53e5d07ae088a1c98
GitHubpawer <144799400+GitHubpawer@users.noreply.github.com>
2023-11-14 23:51:41 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,25 @@
+let num= Math.floor(Math.random()*5);
+
+if(num % 15===0){
+
+console.log('３と５の倍数です');
+
+}
+
+else if(num%3===0){
+
+    console.log('3の倍数です');
+
+}
+
+else if(num%5===0){
+
+    console.log('５の倍数です');
+}
+
+else {
+    console.log(num)
+}
+
+
+
